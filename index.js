73fe2fbc7d4852d3886ad51c3d005dabf5477a42
josephsolomon <jsol71393@gmail.com>
2021-11-29 20:47:35 -0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
-console.log(wordBank[0]); 
-const userInput = prompt.question("What is your name");
-console.log(userInput)
+
+console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
+
+let answer = wordBank[Math.floor(Math.random()*wordBank.length)].toLowerCase();
+//while loop will control number of guesses 
+const startGame  = 
+
+
+
+const userInput = prompt.question("First Guess");
+
+
+
+const guessCheck = (guess, word) => {
+
+    return word.includes(guess);
+
+};
+
+
+
+console.log("userInput", userInput)
